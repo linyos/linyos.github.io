514733048f9f38edd79002a984ad565341676b58
linyos <laso85143@gmail.com>
2022-05-02 15:51:07 +0800

    fix

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
 function openNav(){
+    document.getElementById("main").style.marginLeft="25%";
     document.getElementById("mySidenav").style.width="25%";
+    document.getElementById("mySidenav").style.display="block";
+    document.getElementById("openNav").style.display = 'none';
 }
 
 
 function closeNav(){
-
-    document.getElementById("mySidenav").style.width="0%";
+    document.getElementById("main").style.marginLeft="0%";
+    document.getElementById("mySidenav").style.display="none";
+    document.getElementById("openNav").style.display="inline-block";
 }
